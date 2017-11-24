73357978aa143206ca07e60ddebf03743536dfd7
vinicius silvestre <viniciussilvestre@MacBook-Air-de-vinicius.local>
2017-11-24 18:54:42 -0200

    Initial Commit

---
+++ Reading Diary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reading Diary
+//
+//  Created by vinicius silvestre on 24/11/17.
+//  Copyright © 2017 vinicius silvestre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
